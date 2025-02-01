f8c15c6ea08bb432105f5db710d3660013353db9
Liam Young <liam@geckoengage.com>
2025-02-01 22:49:04 +0000

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gecko-ai/', // Your repository name
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
